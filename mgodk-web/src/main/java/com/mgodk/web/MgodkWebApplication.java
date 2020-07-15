243ed5d28a52c1aa277285d1a9492e33264a6e59
卍 <45060127+wjj57@users.noreply.github.com>
2020-07-15 10:46:57 +0800

    create module

---
+++ mgodk-web/src/main/java/com/mgodk/web/MgodkWebApplication.java
@@ -0,0 +1,24 @@
+package com.mgodk.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Package com.mgodk.web
+ * @ClassName MgodkWebApplication
+ * @Description
+ * @Author WJJ
+ * @Date 2020/7/15 10:06
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class MgodkWebApplication {
+    private static Logger logger = LoggerFactory.getLogger(MgodkWebApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(MgodkWebApplication.class,args);
+        logger.info("MgodkWebApplication ===》 启动成功！");
+    }
+}
