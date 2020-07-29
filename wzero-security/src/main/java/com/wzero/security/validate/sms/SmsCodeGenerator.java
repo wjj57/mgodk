2f59791725ad9a66ebb09a1f468b7ea9112808d8
wangjianjie <wangjianjie@yaspeed.com>
2020-07-29 18:00:53 +0800

    commit

---
+++ wzero-security/src/main/java/com/wzero/security/validate/sms/SmsCodeGenerator.java
@@ -0,0 +1,11 @@
+package com.wzero.security.validate.sms;
+
+/**
+ * @ClassName SmsCodeGenerator
+ * @Description
+ * @Author WJJ
+ * @Date 2020/7/29 18:00
+ * @Version 1.0
+ */
+public class SmsCodeGenerator {
+}
