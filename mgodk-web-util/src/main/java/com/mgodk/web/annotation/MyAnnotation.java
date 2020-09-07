8d4180108ea9e8ae54335e572ce5e95509e7d79f
wjj <wjj@com>
2020-09-07 16:48:28 +0800

    commit

---
+++ mgodk-web-util/src/main/java/com/mgodk/web/annotation/MyAnnotation.java
@@ -0,0 +1,11 @@
+package com.mgodk.web.annotation;
+
+/**
+ * @ClassName MyAnnotation
+ * @Description
+ * @Author WJJ
+ * @Date 2020/09/07 16:34
+ * @Version 1.0
+ */
+public class MyAnnotation {
+}
---
+++ mgodk-web-util/src/main/java/com/mgodk/web/config/MyConfig.java
@@ -0,0 +1,11 @@
+package com.mgodk.web.config;
+
+/**
+ * @ClassName MyConfig
+ * @Description
+ * @Author WJJ
+ * @Date 2020/09/07 16:35
+ * @Version 1.0
+ */
+public class MyConfig {
+}
